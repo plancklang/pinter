211ff989d3886e43cef34dcbbcabd95e860e3815
adamhutchings <adam.abahot@gmail.com>
2020-11-25 13:54:51 -0800

    Add decode function

---
+++ src/decode.c
@@ -0,0 +1,48 @@
+#include "decode.h"
+
+bytecode_t* decode(FILE* fp, int* ct, int* err) {
+
+	bytecode_t* ret;
+
+	if (fp == NULL) {
+		*err = ERR_NULL_FILE;
+		return ret;
+	}
+
+	/* Get the size of the file */
+	fseek(fp, 0L, SEEK_END);
+	int size = ftell(fp);
+	rewind(fp);
+
+	/* Get contents of file (nice) */
+	char contents[size - 1];
+	for (int i = 0; i < size - 1; contents[i] = getc(fp), i++);
+
+	if ( (size % 9) != 1 ) {
+		*err = ERR_PARTIAL_INSTRUCTION;
+		return ret;
+	}
+
+	ret = malloc( (size / 9) * sizeof(bytecode_t) );
+	*ct = size / 9;
+
+	for (int i = 0; i < *ct; i++) {
+
+		/* Set every bytecode */
+
+		/* Check if the opcode is allowed */
+		if ( ((opcode_t) (contents[ (i * 9) ])) >= BYTECODE_MAX ) {
+			*err = ERR_INVALID_OPCODE;
+			return ret;
+		}
+
+		ret[i].opcode = (opcode_t) (contents[ (i * 9) ]);
+		ret[i].left = (operand_t) (contents[ (i * 9) + 1 ]);
+		ret[i].right = (operand_t) (contents[ (i * 9) + 5 ]);
+
+	}
+
+	*err = 0;
+	return ret;
+
+}
---
+++ src/decode.h
@@ -0,0 +1,22 @@
+/**
+ * This file provides the utility of opening a text file and producing an array
+ * of bytecodes for the use of the VM.
+ *
+ * Author: Adam Hutchings
+ */
+
+#include <stdio.h> /* FILE */
+#include <stdlib.h> /* malloc */
+
+#include <src/bytecode/bytecodes.h> /* bytecode_t, etc. */
+
+#define ERR_NULL_FILE -1 /* If a file doesn't exist */
+#define ERR_PARTIAL_INSTRUCTION -2 /* If the byte count is not a * of 9 */
+#define ERR_INVALID_OPCODE -3 /* If the opcode is currently unsupported */
+
+/**
+ * Decode the file given by fp and produce an array of bytecodes. Will set *err
+ * and return if something unexpectedly goes wrong. Also, ct will be set to the
+ * number of bytecodes in the list.
+ */
+bytecode_t* decode(FILE* fp, int* ct, int* err);
