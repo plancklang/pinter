9e545e8e942f49c4bd3cef9c8f2b6b806360f8ce
adamhutchings <adam.abahot@gmail.com>
2020-11-25 13:54:32 -0800

    Add bytecode types

---
+++ src/bytecode/bytecodes.h
@@ -0,0 +1,30 @@
+/**
+ * This file defines the bytecode type, and related types (such as the opcode
+ * type).
+ *
+ * Author: Adam Hutchings
+ */
+
+#ifndef PINTER_BYTECODE_BYTECODES_H
+#define PINTER_BYTECODE_BYTECODES_H
+
+#include <stdint.h> /* int32_t */
+
+typedef enum {
+
+	NXLL, /* Only NXLL for now, nothing else needed */
+	BYTECODE_MAX, /* Put this at the end of the list always */
+
+} opcode_t;
+
+typedef int32_t operand_t; /* Each operand is four bytes. */
+
+typedef struct {
+
+	opcode_t opcode;
+	operand_t left;
+	operand_t right;
+
+} bytecode_t;
+
+#endif /* PINTER_BYTECODE_BYTECODES_H */
