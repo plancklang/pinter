34ab6f1a831d5f5f4b62a1bce13a135251fe302c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-11-24 18:48:22 -0800

    Add usage function

--- src/main.c
+++ src/main.c
@@ -7,13 +7,22 @@
 #include <src/parse_args.h> /* pi_parse, pinter_args_t */
 #include <src/util.h> /* PINTER_ERROR, PL_SUCCESS */
 
+void usage() {
+	printf("\
+Usage:\n\
+	--help see this message\n\
+	[file]: execute [file]\n\
+");
+}
+
+
 int main(int argc, char** argv) {
 
 	--argc, ++argv; /* "pinter" itself doesn't need to be parsed */
 	pinter_args_t args = pi_parse(argc, argv);
 
 	if (args.help) {
-		printf("usage:\n\n\t--help: see this message\n\t[file]: execute [file]\n\n");
+		usage();
 	} else {
 		PINTER_ERROR("bytecode interpretation currently unsupported.\n");
 	}
