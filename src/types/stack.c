75d38d365f516986a0fd8d23bef70ba2c1edd40c
adamhutchings <adam.abahot@gmail.com>
2020-11-30 11:35:26 -0800

    Add stack type

---
+++ src/types/stack.c
@@ -0,0 +1,50 @@
+#include "stack.h"
+
+void pi_stack_init(pi_stack_t* in) {
+
+	in = malloc( sizeof(pi_stack_t) );
+
+	in->content = NULL;
+	in->prev = NULL;
+	in->len = 1;
+
+}
+
+int pi_stack_push(pi_stack_t** in, void* val) {
+
+	pi_stack_t* new_stack; /* new stack member */
+	new_stack->content = val;
+	new_stack->prev = *in;
+
+	/* Check length */
+	if (*in->len >= STACK_MAX_SIZE) {
+		return -1;
+	}
+
+	new_stack->len = *in->len + 1;
+
+	*in = new_stack;
+
+	return 0;
+
+}
+
+void* pi_stack_pop(pi_stack_t** in, int* err) {
+
+	if (*in->len == 0) {
+		*err = -1;
+		return NULL;
+	}
+
+	void* ret = *in->content;
+
+	/* Copy for freeing up */
+	pi_stack_t* to_free = *in;
+
+	*in = *in->prev;
+
+	free(to_free);
+
+	return ret;
+
+}
---
+++ src/types/stack.h
@@ -0,0 +1,43 @@
+/**
+ * Implementation of a stack for Planck. Uses void pointers for ease of using
+ * different types.
+ *
+ * Author: Adam Hutchings
+ */
+
+#ifndef PINTER_TYPES_STACK_H
+#define PINTER_TYPES_STACK_H
+
+#include <stdlib.h> /* malloc, free */
+
+#define STACK_MAX_SIZE (1 << 8); /* ~ 250 ~/
+
+/**
+ * Contains some "content", and a pointer to the previous element.
+ */
+struct pi_stack_s {
+
+	void* content; /* Could be anything. Calling code must check. */
+	struct pi_stack_s* prev; /* Is NULL if this is the "bottom". */
+	int len; /* The current size of this stack. */
+
+};
+typedef struct pi_stack_s pi_stack_t;
+
+/**
+ * Initialize the stack with a bottom (meaningless) member
+ */
+void pi_stack_init(pi_stack_t* in);
+
+/**
+ * Push a value onto the stack. Return -1 if unsuccessful (max size exceeded)
+ */
+int pi_stack_push(pi_stack_t* in, void* val);
+
+/**
+ * Pop a value from the stack and return it. Set *err to -1 if bottom of the
+ * stack is reached.
+ */
+void* pi_stack_pop(pi_stack_t* in, int* err);
+
+#endif /* PINTER_TYPES_STACK_H */
